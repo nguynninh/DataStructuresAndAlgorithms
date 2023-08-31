529b1f045099abd5e92de0f78bbb6d03ffae3fc1
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2023-08-31 22:01:31 +0700

    Tree - BinarySearchTree

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -3,15 +3,13 @@ package sort;
 public class InsertionSort implements Sort {
     @Override
     public void sort(int[] array) {
-        int key, j;
         for (int i = 0; i < array.length; i++) {
-            key = array[i];
-            j = i - 1;
-            while (j >= 0 && array[j] > key) {
+            int j = i - 1;
+            while (j >= 0 && array[j] > array[i]) {
                 array[j + 1] = array[j];
                 j -= 1;
             }
-            array[j + 1] = key;
+            array[j + 1] = array[i];
         }
     }
 }
--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,4 +1,13 @@
 package tree;
 
-public interface Tree {
+import list.Iterator;
+
+public interface Tree<T> {
+    boolean isEmpty();
+    int size();
+    int height();
+    boolean contains(T element);
+    boolean add(T element);
+    boolean remove(T element);
+    Iterator<T> traverse(TreeTraverseType type);
 }
